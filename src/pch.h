d0a12c2d7ae42cba7b1a993b6d69dc6505420045
rdbo <rdbodev@gmail.com>
2021-01-09 04:16:53 -0300

    initial commit

---
+++ src/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef PCH_H
+#define PCH_H
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <Windows.h>
+#include <WinSock2.h>
+#include <assaultcube/sdk.h>
+
+#endif
